863ea48e7258e149c094a39dc9b1a2618f4b3ac7
Guilherme <gui25ber@gmail.com>
2020-03-27 16:38:02 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.get('/', (request,response) => {
 
-    return response.send("Hello World");
-})
+    return response.json({
+        evento: 'semana OmniStak 11.0',
+        aluno: 'Guilherme Bernardo'
+    });
+});
 app.listen(3333);
