178788e89930193b650c8a59020067be05e316a0
Guilherme <gui25ber@gmail.com>
2020-03-27 18:40:46 -0300

    update

--- project/backend/index.js
+++ project/backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request,response) => {
+app.post('/users', (request,response) => {
 
     return response.json({
         evento: 'semana OmniStak 11.0',
