56a654401c7101da23f3bef30e3e32fca1e522c3
Guilherme <gui25ber@gmail.com>
2020-03-29 19:02:06 -0300

    upgrade

--- project/backend/src/database/connection.js
+++ project/backend/src/database/connection.js
@@ -1,6 +1,12 @@
-const knex = require('knex');
-const configuration = require('../../knexfile');
+const knex = require('knex')
+const configurarion = require('../../knexfile')
 
-const connection = knex(configuration.development);
+const config = process.env.NODE_ENV === 'test' ? 
+    configurarion.test 
+  : configurarion.development;
+
+console.log('Variável de ambiente: '+process.env.NODE_ENV)
+
+const connection = knex(config)
 
 module.exports = connection;
--- project/backend/src/routes.js
+++ project/backend/src/routes.js
@@ -1,25 +1,79 @@
-const express = require('exprees');
-const crypto = require('crypto');
-const connection = require('./database/connection')
+const express = require('express')
+const { celebrate,Segments, Joi } = require('celebrate')
 
 const routes = express.Router();
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 
-routes.post('/ongs', async (request,response) => {
+routes.post('/sessions', celebrate({
 
-    const {name, email, whatsapp, city, uf} = request.body;
-    
-    const id = crypto.randomBytes(4).toString(HEX);
-    
-    await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-    });
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required(),
+    }),
 
-    return response.json({ id });
-});
+}), SessionController.create)
+
+
+
+routes.get('/ongs',  OngController.index)
+
+routes.post('/ongs', celebrate({
+
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(13),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+
+}), OngController.create)
+
+
+routes.post('/incidents', celebrate({
+
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required().min(5),
+        description: Joi.string().required().min(10),
+        value: Joi.number().min(0),
+    }),
+
+}), IncidentController.create)
+
+routes.get('/incidents',  celebrate({
+
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+
+}),
+IncidentController.index)
+
+routes.delete('/incidents/:id', celebrate({
+
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    }),
+
+}),IncidentController.delete)
+
+
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown()
+}), ProfileController.index)
+
 
 module.exports = routes;
