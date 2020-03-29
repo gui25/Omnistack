a238250a016c70d0a230a708f50c7f694da1416b
Guilherme <gui25ber@gmail.com>
2020-03-29 18:07:25 -0300

    upgrade

---
+++ project/backend/src/database/connection.js
@@ -0,0 +1,6 @@
+constg knex = require('knex');
+const configuration = require('../../knexfile');
+
+const connection = knex(configuration.development);
+
+module.exports = connection;
---
+++ project/backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('exprees');
+const crypto = require('crypto');
+const connection = require('./database/connection')
+
+const routes = express.Router();
+
+routes.post('/ongs', (request,response) => {
+
+    const {name, email, whatsapp, city, uf} = request.body;
+    
+    const id = crypto.randomBytes(4).toString(HEX);
+    
+    
+
+    return response.json({
+        
+    });
+});
+
+module.exports = routes;
